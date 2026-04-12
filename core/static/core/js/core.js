794bfb60b99e5e76da58ce4b9dc3c5fdd71487da
ralatigano <ramirolatigano@gmail.com>
2026-04-11 21:25:32 -0300

    Se cambia la estructura del navbar.

--- core/static/core/js/core.js
+++ core/static/core/js/core.js
@@ -88,28 +88,26 @@ themeButtons.forEach(button => {
 // contrasten apropiadamente con el tema elegido.
 function editThemeToggleMenu(themeValue) {
     // Obtener los elementos de las marcas de verificación
-    var themeLightCheckbox = document.getElementById("theme-light");
-    var themeDarkCheckbox = document.getElementById("theme-dark");
-    
-    // Obtener el elemento del botón y los íconos
-    var themeButton = document.getElementById("theme-button");
-    if (themeButton) {
-        // Mostrar u ocultar las marcas de verificación correspondientes
-        if (themeValue === "light") {
-            themeLightCheckbox.style.display = "block";
-            themeDarkCheckbox.style.display = "none";
-        } else if (themeValue === "dark") {
-            themeLightCheckbox.style.display = "none";
-            themeDarkCheckbox.style.display = "block";
-        }
-        // Cambiar la clase del botón para cambiar tema según el tema seleccionado
-        themeButton.classList.remove("bi-sun-fill", "bi-moon-stars-fill", "bi-circle-half-fill");
-        themeButton.classList.add(getIconClass(themeValue));
-        
-        // Actualizar el color de los SVG según el tema seleccionado
-        setSvgsImages(themeValue);
+    var themeLightCheckbox = document.getElementById("check-light");
+    var themeDarkCheckbox = document.getElementById("check-dark");
+    var themeLightCheckboxDesktop = document.getElementById("check-light-desktop");
+    var themeDarkCheckboxDesktop = document.getElementById("check-dark-desktop");
+
+    // Mostrar u ocultar las marcas de verificación correspondientes
+    if (themeValue === "light") {
+        themeLightCheckbox?.classList.remove("d-none");
+        themeDarkCheckbox?.classList.add("d-none");
+        themeLightCheckboxDesktop?.classList.remove("d-none");
+        themeDarkCheckboxDesktop?.classList.add("d-none");
+    } else if (themeValue === "dark") {
+        themeLightCheckbox?.classList.add("d-none");
+        themeDarkCheckbox?.classList.remove("d-none");
+        themeLightCheckboxDesktop?.classList.add("d-none");
+        themeDarkCheckboxDesktop?.classList.remove("d-none");
     }
+
 }
+
 // Función que modifica el ícono del botón de tema
 function getIconClass(themeValue) {
     if (themeValue === "light") {
@@ -118,33 +116,7 @@ function getIconClass(themeValue) {
         return "bi-moon-stars-fill";
     } 
 }
-// Función que modifica algunas cuestiones visuales del menú desplegable del cambio de tema como los dibujos y el checkk que indica el tema elegido.
-function setSvgsImages(themeValue) {
-    const svgSun = document.querySelectorAll('.bi-sun-fill');
-    svgSun.forEach(svg => {
-      if (themeValue === "light") {
-        svg.style.backgroundImage = `url("data:image/svg+xml,<svg viewBox='0 0 16 16' fill='%23332F2E' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708' clip-rule='evenodd'/></svg>")`;
-      } else if (themeValue === "dark") {
-        svg.style.backgroundImage = `url("data:image/svg+xml,<svg viewBox='0 0 16 16' fill='%23C2C4C8' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708' clip-rule='evenodd'/></svg>")`;
-      }
-    });
-    const svgMoon = document.querySelectorAll('.bi-moon-stars-fill');
-    svgMoon.forEach(svg => {
-      if (themeValue === "light") {
-        svg.style.backgroundImage = `url("data:image/svg+xml,<svg viewBox='0 0 16 16' fill='%23332F2E' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278' clip-rule='evenodd'/><path fill-rule='evenodd' d='M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z' clip-rule='evenodd'/></svg>")`;
-      } else if (themeValue === "dark") {
-        svg.style.backgroundImage = `url("data:image/svg+xml,<svg viewBox='0 0 16 16' fill='%23C2C4C8' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278' clip-rule='evenodd'/><path fill-rule='evenodd' d='M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.73 1.73 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.73 1.73 0 0 0 1.097-1.097zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z' clip-rule='evenodd'/></svg>")`;
-      }
-    });
-    const svgCheck = document.querySelectorAll('.bi-check2');
-    svgCheck.forEach(svg => {
-      if (themeValue === "light") {
-        svg.style.backgroundImage = `url("data:image/svg+xml,<svg viewBox='0 0 16 16' fill='%23332F2E' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0' clip-rule='evenodd'/></svg>")`;
-      } else if (themeValue === "dark") {
-        svg.style.backgroundImage = `url("data:image/svg+xml,<svg viewBox='0 0 16 16' fill='%23C2C4C8' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0' clip-rule='evenodd'/></svg>")`;
-      }
-    });
-  }
+
 
 // Función que modifica la clase de un div en el template base para cambiar la imagen de fondo.
 function setBackgroundImageAndButtons(themeValue) {
